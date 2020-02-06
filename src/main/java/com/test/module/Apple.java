8b8b2d79aaf467f010921954a2059e35b5f35809
unknown <zhouna@ESG.360ES.CN>
2020-02-06 13:30:27 +0800

    jenkins test

---
+++ src/main/java/com/test/module/Apple.java
@@ -0,0 +1,13 @@
+package com.test.module;
+
+public class Apple {
+    private String name = "aa";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page import="com.test.module.Apple" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<%=new Apple().getName()%>
+</body>
+</html>
